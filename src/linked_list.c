5d0865b266807b9c3be8409e746d9f6d68d72e46
Nathaniel D'Rozario <48631513+nsdrozario@users.noreply.github.com>
2020-08-21 00:07:02 -0700

    Create linked_list.c

---
+++ src/linked_list.c
@@ -0,0 +1,36 @@
+#include <linked_list.h>
+
+void linked_list_add_to_end(linked_list* l, node *n) {
+
+  node *current_node = l->start;
+  size_t size = 0;
+  while(current_node->next != NULL) {
+    size++;
+    current_node = current_node->next;
+  }
+  current_node->next = n;
+  size++;
+
+}
+
+// add null pointer guards to these functions later
+void linked_list_remove_node(linked_list *l, int n) { // joins parent of nth element with child of nth element while removing nth element from the linked list
+  
+  node *parent_node = l->start;
+  for (int i = 0; i < n-1; i++) {
+    parent_node = parent_node->next;
+  }
+  parent_node->next = parent_node->next->next;
+  
+}
+
+void linked_list_remove_recursive(linked_list *l, int n) { // this function isn't actually recursive it just deletes every node from the given index
+
+  node *current_node = l->start;
+  for (int i = 0; i < n; i++) {
+    current_node = current_node->next;
+  }
+  
+  current_node->next = NULL;
+  
+}
