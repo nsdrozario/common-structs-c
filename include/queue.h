24926b8b911ffdf636a51156b0c11cab3e392dae
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-08-21 13:29:04 -0700

    implemented queue

--- include/queue.h
+++ include/queue.h
@@ -10,12 +10,23 @@ extern "C" {
 typedef struct {
 
     void **contents;
+    size_t allocated_size = 0;
+    size_t real_size = 0;
+    int front_index = 0;
 
 } queue;
 
+// constructors
+queue *create_queue();
+queue *create_queue(size_t s);
+
+// destructors
+void delete_queue(queue *q);
+
 // operations
-void *queue_front();
-void *queue_pop();
+void *queue_front(queue *q);
+void queue_pop(queue *q);
+void queue_push(void *element);
 
 #ifdef __cplusplus
 }
---
+++ src/queue.c
@@ -0,0 +1,37 @@
+#include <queue.h>
+
+queue *create_queue() {
+
+    queue *q = malloc(sizeof(queue));
+    q->contents = malloc(0);
+    q->allocated_size = 0;
+    q->real_size = 0;
+
+}
+
+queue *create_queue(size_t s) {
+
+    queue *q = malloc(sizeof(queue));
+    q->contents = malloc(sizeof(void *) * s);
+    q->allocated_size = s;
+    q->real_size = 0;
+
+}
+
+void *queue_front(queue *q) {
+
+    return q->contents[q->front_index];
+
+}
+
+void queue_pop(queue *q) {
+
+    q->front_index++;
+
+}
+
+void delete_queue(queue *q) {
+
+    free(q->contents);
+
+}
