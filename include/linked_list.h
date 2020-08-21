91eae64cc09358041ba3f4781213653e1ab5b6e8
Nathaniel D'Rozario <48631513+nsdrozario@users.noreply.github.com>
2020-08-21 00:07:45 -0700

    Update linked_list.h

--- include/linked_list.h
+++ include/linked_list.h
@@ -18,40 +18,11 @@ size_t size;
 
 } linked_list;
 
-void linked_list_add_to_end(linked_list* l, node *n) {
+void linked_list_add_to_end(linked_list* l, node *n);
 
-  node *current_node = l->start;
-  size_t size = 0;
-  while(current_node->next != NULL) {
-    size++;
-    current_node = current_node->next;
-  }
-  current_node->next = n;
-  size++;
+void linked_list_remove_node(linked_list *l, int n);
 
-}
-
-// add null pointer guards to these functions later
-void linked_list_remove_node(linked_list *l, int n) { // joins parent of nth element with child of nth element while removing nth element from the linked list
-  
-  node *parent_node = l->start;
-  for (int i = 0; i < n-1; i++) {
-    parent_node = parent_node->next;
-  }
-  parent_node->next = parent_node->next->next;
-  
-}
-
-void linked_list_remove_recursive(linked_list *l, int n) { // this function isn't actually recursive it just deletes every node from the given index
-
-  node *current_node = l->start;
-  for (int i = 0; i < n; i++) {
-    current_node = current_node->next;
-  }
-  
-  current_node->next = NULL;
-  
-}
+void linked_list_remove_recursive(linked_list *l, int n);
 
 #ifdef __cplusplus
 }
