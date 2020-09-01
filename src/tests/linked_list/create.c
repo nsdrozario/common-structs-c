ded80f2c8fa543b2177d9828d373546f3bc7d94a
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-08-31 21:12:58 -0700

    linked list constructor/destructor test added

---
+++ src/tests/linked_list/create.c
@@ -0,0 +1,22 @@
+#include <linked_list.h>
+#include <stdio.h>
+
+int main () {
+
+    linked_list *l = linked_list_create();
+    linked_list *l0 = l;
+    l->value = 0;
+    for (int i = 0; i < 10; i++) {
+        linked_list_push_back(l, i);
+    }
+
+    while(l->next != NULL) {
+        printf("%d ", l->value);
+        l = l->next;
+    }
+
+    linked_list_delete(l0);
+
+    return 0;
+
+}
