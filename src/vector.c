f913e040bc3b3ad15901cfc70a7fb2acd662f679
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-09-10 23:28:07 -0700

    Implemented error handling to vector structure

--- src/vector.c
+++ src/vector.c
@@ -1,51 +1,78 @@
 #include <common_structs_c/vector.h>
 #include <limits.h>
 
-vector *vector_create(size_t s) {
+vector *vector_create(size_t s, uint8_t *error) {
 
     vector *v = malloc(sizeof(vector));
+
+    if (v == NULL) {
+        *error = VECTOR_ALLOC_ERROR;
+        return NULL;
+    }
+
     v->contents = calloc(s, sizeof(int));
     v->allocated_size = s;
     v->size = 0;
+    *error = 0;
     return v;
 
 }
 
-void vector_resize(vector *v, size_t s) {
+void vector_resize(vector *v, size_t s, uint8_t *error) {
 
-    v->contents = realloc(v->contents, s * sizeof(int));
+    int *tmp = realloc(v->contents, s * sizeof(int));
+    if (tmp == NULL) {
+        free(v->contents);
+        v->allocated_size = 0;
+        *error = VECTOR_ALLOC_ERROR;
+        return;
+    }
+
+    *error = 0;
+    v->contents = tmp;
     v->allocated_size = s;
 
 }
 
-void vector_push_back(vector *v, int element) {
+void vector_push_back(vector *v, int element, uint8_t *error) {
 
     if (v->allocated_size == v->size) { 
-        vector_resize(v, ((v->allocated_size)+1) * sizeof(int)); 
+        uint8_t e = 0;
+        vector_resize(v, ((v->allocated_size)+1), &e);
+        if (e == -1) {
+            *error = VECTOR_ALLOC_ERROR;
+            return;
+        }
     }
 
     v->contents[v->size] = element;
     v->size++;
+    *error = 0;
 
 }
 
-int vector_get(vector *v, int index) {
+int vector_get(vector *v, int index, uint8_t *error) {
 
     if (index >= v->size) {
-        return INT_MIN;
+        
+        *error = VECTOR_INDEX_ERROR;
+        return NULL;
+
     }
 
     return v->contents[index];
 
 }
 
-void vector_set(vector *v, int index, int val) {
+void vector_set(vector *v, int index, int val, uint8_t *error) {
     
     if (index >= v->allocated_size) {
+        *error = VECTOR_INDEX_ERROR;
         return;
     }
 
     v->contents[index] = val;
+    *error = 0;
 
 }
 
