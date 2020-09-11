0b15b69a412ba0393401b8ef7609591045da9389
Nathaniel D'Rozario <nsdrozario@gmail.com>
2020-09-10 23:34:09 -0700

    Updated test code to work with new functions

--- src/tests/vector/push_back.c
+++ src/tests/vector/push_back.c
@@ -3,15 +3,31 @@
 
 int main () {
 
-    vector *v = vector_create(0);
+    uint8_t create_vector_error = 0;
+    vector *v = vector_create(0, &create_vector_error);
 
+    if (!create_vector_error) {
+        printf("Vector creation failed, exiting...\n");
+        exit(-1);
+    }
+
+    uint8_t push_back_error = 0;
     for (int i = 0; i < 10; i++) {
-        vector_push_back(v, i);
+        vector_push_back(v, i, &push_back_error);
+        if (!push_back_error) {
+            printf("Realloc failed while using vector_push_back(), exiting...\n");
+            exit(-1);
+        }
     }
 
     for (int i = 0 ; i < 10; i++) {
 
-        printf("%d ", v->contents[i]);
+        uint8_t vector_index_error = 0;
+
+        printf("%d ", vector_get(v, i, &vector_index_error));
+        if (vector_index_error == VECTOR_INDEX_ERROR) {
+            printf("Invalid index for accessing vector contents\n");
+        }
 
     }
 
